06e6c6565aef9af39640d05429afc96af095332d
日暮竜太 <ryo.ddd@gmail.com>
2019-09-29 14:44:43 +0900

    installed sample files

---
+++ node.js%EB%B3ƨsample/ex-gen-app/app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// add session
+var session = require('express-session');
+
+var session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false, 
+  cookie: { maxAge: 60 * 60 * 1000 }
+};
+app.use(session(session_opt));
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// validator
+var validator = require('express-validator');
+app.use(validator());
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// add hello.
+var hello = require('./routes/hello');
+app.use('/hello', hello);
+
+var ajax = require('./routes/ajax');
+app.use('/ajax', ajax);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
