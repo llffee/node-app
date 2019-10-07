40351c97b5c1c5e7d2e2b5a2b43917eb777fd401
ryo <ryota.ddd@gmail.com>
2019-10-07 23:28:57 +0900

    added mini_board

--- app.js
+++ app.js
@@ -42,11 +42,10 @@ function getFromClient(request,response){
 
 var data = {msg:'no message...'};
 
-// access transaction
+// indexのアクセス処理
 function response_index(request, response) {
     // access throgh POST 
     if (request.method == 'POST') {
-        
         var body='';
         
         // receive data
@@ -57,6 +56,7 @@ function response_index(request, response) {
         // end of receiving data
         request.on('end',() => {
             data = qs.parse(body);
+            setCookie('msg',data.msg, response);
             write_index(request, response);
         });
     } else {
@@ -67,12 +67,32 @@ function response_index(request, response) {
 // index
 function write_index(request, response) {
     var msg = "※伝言を表示します。"
+    var cookie_data = getCookie('msg', request);
     var content = ejs.render(index_page, {
         title:"Index",
         content:msg,
         data:data,
+        cookie_data:cookie_data,
     });
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(content);
     response.end();
+}
+
+// クッキーの値を設定
+function setCookie(key, value, response) {
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+function getCookie(key, request) {
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data){
+        if (data[i].trim().startsWith(key + '=')) {
+            var result = data[i].trim().substring(key.length+1);
+            return unescape(result);
+        }
+    }
+    return '';
 }
