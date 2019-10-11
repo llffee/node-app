c89975a018022f98fb44c6e5b9feec61d561ffd5
ryo <ryota.ddd@gmail.com>
2019-10-12 00:26:02 +0900

    added stylesheet

--- express-app/index.js
+++ express-app/index.js
@@ -1,10 +1,20 @@
-var express = require('express')
+var express = require('express');
+var ejs = require("ejs");
+
 var app = express()
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Express!')
-})
+app.engine('ejs',ejs.renderFile);
 
-app.listen(3000, () => {
-    console.log('Start server port:3000')
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    var msg = 'This is Express Page!<br>' + 'これは、スタイルシートを利用した例です。';
+    // index.ejsをレンダリングする
+    res.render('index.ejs',
+        {title: 'Index',
+            content: msg});
+});
+
+var server = app.listen(3000, () => {
+    console.log('Server is running!');
 })
