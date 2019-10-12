df13a6e1c0e53cedf0e9b6c7665400769164003e
ryo <ryota.ddd@gmail.com>
2019-10-12 13:46:25 +0900

    commit files in experess folder

--- express-app/index.js
+++ express-app/index.js
@@ -2,17 +2,39 @@ var express = require('express');
 var ejs = require("ejs");
 
 var app = express()
-
 app.engine('ejs',ejs.renderFile);
-
 app.use(express.static('public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+var data = {
+    'Taro':'taro@yamada',
+    'Hanako':'hanako@flower',
+    'Sachiko':'sachiko@happy',
+    'Ichiro':'ichiro@baseball',
+}
+
 app.get('/', (req, res) => {
-    var msg = 'This is Express Page!<br>' + 'これは、スタイルシートを利用した例です。';
+    var msg = 'This is Index Page!<br>' + '※データを表示します。';
+    var url = 'other?name=taro&pass=yamada';
     // index.ejsをレンダリングする
     res.render('index.ejs',
-        {title: 'Index',
-            content: msg});
+        {
+            title: 'Index',
+            content: msg,
+            data: data,
+        });
+});
+
+// POST送信の処理
+app.post('/',(req, res) => {
+    var msg = 'This is Posted Page!<br>' + 'あなたは「<b>' + req.body.message + '<b>」と送信しました。';
+    res.render('index.ejs',
+    {
+        title: 'Posted',
+        content: msg,
+    });
 });
 
 var server = app.listen(3000, () => {
